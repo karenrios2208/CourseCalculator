53ddb4e348346d34d7e17517b750969ec16a554f
Jazmin Santibáñez <a01234029@itesm.mx>
2021-06-09 19:52:59 -0500

    Reorganize template with require

---
+++ Resources/templates/sidebar.php
@@ -0,0 +1,47 @@
+<div class="sidebar">
+  <div class="logo_content">
+    <div class="logo">
+      
+    </div>
+    <i class='bx bx-menu' id="btn" ></i>
+  </div>
+  <ul class="nav_list">
+    <li>
+      <a href="#">
+        <i class='bx bx-home'  ></i>
+        <span class="links_name">Home</span>
+      </a>
+      <span class="tooltip">Home</span>
+    </li>
+    <li>
+      <a href="../Views/settings.php">
+        <i class='bx bx-user' ></i>
+        <span class="links_name">Account</span>
+      </a>
+      <span class="tooltip">Account</span>
+    </li>
+    <li>
+      <a href="#">
+        <i class='bx bx-calculator'  ></i>
+        <span class="links_name">Grade Simulator</span>
+      </a>
+      <span class="tooltipBig">Grade Simulator</span>
+    </li>
+    <li>
+      <a href="../Views/plan.php">
+        <i class='bx bx-pie-chart-alt-2' ></i>
+        <span class="links_name">Academic Plan</span>
+      </a>
+      <span class="tooltipBig">Academic Plan</span>
+    </li>
+  </ul>
+  <div class="profile_content">
+    <div class="profile">
+      <a href="logout.php">
+          <i class='bx bx-log-out' id="log_out" ></i>
+      </a>
+    </div>
+  </div>
+</div>
+
+</script>
