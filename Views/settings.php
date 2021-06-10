b7a01991f86ba8f2e52e2ff04190d6415a05873d
Jazmin Santibáñez <a01234029@itesm.mx>
2021-06-09 20:54:36 -0500

    Add settings page with card

--- Views/settings.php
+++ Views/settings.php
@@ -8,6 +8,7 @@ session_start();
   <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../Resources/css/navbar.css">
+    <link rel="stylesheet" href="../Resources/css/card.css">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>template</title> 
@@ -20,7 +21,14 @@ session_start();
     <?php
       require '../Resources/templates/topbar.php';
     ?>
-    <div class="title">Title</div>
+    <div class="title">User configuration</div>
+    <div class="creditCardForm">
+      <div class="text">Change name</div>
+      <form id="changeName" action="settings.php"></form>
+      <div class="text">Change photo</div>
+      <div class="text">Change password</div>
+      <div class="text">Change studyplan</div>
+    </div>
   </div>
     <?php
       require '../Resources/templates/sidebar-scripts.php';
